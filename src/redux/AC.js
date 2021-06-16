41cb9ebcf9d0d113a7ded03b68c780884570ae4f
Rynay <feirs911@gmail.com>
2021-06-16 18:41:00 +0300

    signIn signUp logout Actions

--- src/redux/AC.js
+++ src/redux/AC.js
@@ -6,14 +6,52 @@ export const init = () => (dispatch) => {
   const listener = firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       localStorage.setItem('user', JSON.stringify(user));
-      setUser(user);
+      dispatch(setUser(user));
     } else if (localUser) {
-      setUser(localUser);
+      dispatch(setUser(localUser));
     } else {
-      setUser(null);
+      dispatch(setUser(null));
     }
   });
   return () => listener();
 };
 
+export const signIn =
+  ({ email, password }) =>
+  (dispatch) => {
+    try {
+      return firebase.auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      dispatch(setError(error));
+    }
+  };
+export const signUp =
+  ({ email, password, displayName }) =>
+  async (dispatch) => {
+    try {
+      return firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() =>
+          firebase.auth().currentUser.updateProfile({
+            displayName,
+          })
+        );
+    } catch (error) {
+      dispatch(setError(error));
+    }
+  };
+
+export const logout = () => {
+  localStorage.removeItem('user');
+  return firebase.auth().signOut();
+};
+
+const setError = (payload) => ({ type: TYPES.SET_ERROR, payload });
 const setUser = (payload) => ({ type: TYPES.SET_USER, payload });
+export const addToCart = (payload) => ({ type: TYPES.ADD_TO_CART, payload });
+export const removeFromCart = (payload) => ({
+  type: TYPES.REMOVE_FROM_CART,
+  payload,
+});
+export const clearCart = () => ({ type: TYPES.CLEAR_CART });
