8bc2754ecd5b54192d65a485c8c29582cd0897ae
Rynay <feirs911@gmail.com>
2021-08-12 10:01:34 +0300

    pages/NotFound => ts

--- src/pages/NotFound/NotFound.js
+++
@@ -1,5 +0,0 @@
-import s from './NotFound.module.scss';
-
-export const NotFound = () => {
-  return <h1 className={s.warning}>404 Page Not Found</h1>;
-};
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import s from './NotFound.module.scss'
+
+export const NotFound = () => {
+  return <h1 className={s.warning}>404 Page Not Found</h1>
+}
